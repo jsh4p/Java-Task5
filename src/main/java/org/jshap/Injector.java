f13f89910186ed23088ea8dbed31e9497a91a6e1
shap1kUS <j.shapovalov03@icloud.com>
2023-12-07 18:08:59 +0300

    мелкие фиксы + 1 тест

--- src/main/java/org/jshap/Injector.java
+++ src/main/java/org/jshap/Injector.java
@@ -4,12 +4,22 @@ import java.io.*;
 import java.lang.reflect.Field;
 import java.util.Properties;
 
+/**
+ * Класс, который бы осуществлял внедрение зависимостей в любой объект, содержащий поля,
+ * помеченные нашей аннотацией
+ * @author jshap
+ */
 public class Injector {
-    public static Object inject(Object obj, String path) {
+    /**
+     * Метод, осуществляющий внедрение зависимостей
+     * @param obj экземпляр класса
+     * @return ссылка на obj
+     */
+    public static Object inject(Object obj) {
         Field[] fields = obj.getClass().getDeclaredFields();
         Properties properties = new Properties();
 
-        try (InputStream in = Injector.class.getClassLoader().getResourceAsStream(path)) {
+        try (InputStream in = Injector.class.getClassLoader().getResourceAsStream("injector_example.properties")) {
             if (in == null) {
                 throw new NullPointerException();
             }
@@ -21,7 +31,7 @@ public class Injector {
 
         for (Field field : fields) {
             if (field.isAnnotationPresent(AutoInjectable.class)) {
-                String propertyKey = field.getName();
+                String propertyKey = field.getType().getName();
                 String implementationClassName = properties.getProperty(propertyKey);
 
                 if (implementationClassName == null) {
--- src/main/java/org/jshap/Main.java
+++ src/main/java/org/jshap/Main.java
@@ -4,6 +4,6 @@ import org.jshap.example.SomeBean;
 
 public class Main {
     public static void main(String[] args) {
-        ((SomeBean) (Injector.inject(new SomeBean(),"injector_example.properties"))).foo();
+        ((SomeBean) (Injector.inject(new SomeBean()))).foo();
     }
 }
